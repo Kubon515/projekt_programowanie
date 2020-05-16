54bd734112b4bb3a25b02373ca5f55373cc95496
TheIronBirch <65417436+TheIronBirch@users.noreply.github.com>
2020-05-16 17:16:47 +0200

    Update main.cpp
    Full working version of the game

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 /*! \file main.cpp
 *
-* \author Jakub Rosinski
-* \date 2020.05.15
+* \author Jakub Rosinski, Michał Skowroński 
+* \date 2020.05.15, 2020.05.16
 * \version 1.00.00
 */
 
 #include <iostream>
 #include "windows.h"
+#include "time.h"
 
 using namespace std;
 
@@ -37,18 +38,27 @@ char map[20][20] = {
 bool endgame = false;
 int gamespeed = 100;
 int randomNum;
+int MaxHealth = 200;
+int yourHealth = MaxHealth;
 
 int main()
 {
+	
+	
+	
 	system("color 0a");
 	while (endgame == false) {
+		
 
+		srand(time(0));
 		system("cls");
 		for (int y = 0; y < 20; y++ ) {
 
 			cout << map[y] << endl;
 
 		}
+		
+		cout<< "Health " << yourHealth << "/" << maxHealth;
 
 		for (int y = 0; y < 20; y++ ) {
 			for (int x = 0; x < 20; x++) {
@@ -104,17 +114,48 @@ int main()
 					}
 					else if (map[y][x] == '@') {
 
-						map[y][x] = ' ';
+					
 
 					}
 
 
 				break;
+						
+					case '@ ':
+					randomNum = rand() %10 +1;
+						
+						if(randomNum == 1) {
+							y++;
+							Map[y][x] = '*';
+						
+						break;
+						
+							case '*':
+							Map[y][x] = '';
+							y++;
+							
+							if(Map[y][x] !='#' && Map[y][x] !='W'){
+							Map[y][x] = '*';
+							}else if (Map[y][x] == 'W') {
+								
+									yourHealth -= 20;
+							}
+							break;
+							
 				}
 			}
 		}
+		if(yourHealth <= 0){
+		
+			endgame = true;
+		}
+			
 		Sleep(gamespeed);
 	}
 
+	system("cls");	
+	cout << "You failed to protect the earth from hostile aliens" <<endl;	
+	
+		system("PAUSE");
 	return 0;
 }
