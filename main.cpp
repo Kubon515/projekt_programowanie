56c9cc225a9f9a54c1c0890d76cdbcc66c185068
oskarw3e4r <65458996+oskarw3e4r@users.noreply.github.com>
2020-06-07 21:01:32 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*! \file main.cpp
 *
-* \author Jakub Rosinski, Michal Skowronski, Oskar Smektala
+* \author Jakub Rosinski, Micha³ Skowroñski . Oskar Smektala
 * \date 2020.05.18
 * \version 1.00.00
 */
@@ -35,26 +35,57 @@ char map[20][20] = {
 
 };
 
-bool endgame = false;
-int gamespeed = 100;
+bool endGame = false;
+bool loose = false;
+int gameSpeed = 200;
 int randomNum;
-int maxhealth = 200;
-int yourhealth = maxhealth;
+int maxHealth = 100;
+int yourHealth = maxHealth;
+int maxLives = 3;
+int lives = maxLives;
+int points = 0;
+HANDLE hConsole;
+COORD leftTopCursorPos = { 0, 0 };
+
+void clearConsole()
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) return;
+	DWORD size = csbi.dwSize.X * csbi.dwSize.Y;
+	DWORD charsWrited;
+	COORD home = { 0, 0 };
+	if (!FillConsoleOutputCharacter(hConsole, (TCHAR)' ', size, home, &charsWrited)) return;
+	if (!GetConsoleScreenBufferInfo(hConsole, &csbi)) return;
+	if (!FillConsoleOutputAttribute(hConsole, csbi.wAttributes, size, home, &charsWrited)) return;
+	SetConsoleCursorPosition(hConsole, home);
+}
+
+void clearBullets()
+{
+	for (int y = 0; y < 20; y++) {
+		for (int x = 0; x < 20; x++) {
+			if (map[y][x] == '*') map[y][x] = ' ';
+		}
+	}
+}
 
 int main()
 {
-	system("color 0a");
-	while (endgame == false) {
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_GREEN);
+
+	while (endGame == false) {
 
 		srand(time(0));
-		system("cls");
+		SetConsoleCursorPosition(hConsole, leftTopCursorPos);
+
 		for (int y = 0; y < 20; y++ ) {
-
 			cout << map[y] << endl;
-
 		}
 
-		cout << "Health " << yourhealth << "/" << maxhealth;
+		cout << "Health " << yourHealth << "/" << maxHealth << endl;
+		cout << "Lives " << lives << "/" << maxLives << endl;
+		cout << "Points " << points << endl;
 
 		for (int y = 0; y < 20; y++ ) {
 			for (int x = 0; x < 20; x++) {
@@ -63,70 +94,62 @@ int main()
 					
 					case 'W':
 
-						if (GetAsyncKeyState(VK_LEFT) != 0) {
+						if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
 
 							int newX = x - 1;
-
-						    switch (map[y][newX]) {
-
-								case ' ':
+						    if (map[y][newX] == ' ') {
 								map[y][x] = ' ';
 								x--;
 								map[y][newX] = 'W';
-								break;
-								}
+							}
 
 						}
-						if (GetAsyncKeyState(VK_RIGHT) !=0) {
+						if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
 
 							int newX = x + 1;
-
-							switch (map[y][newX]) {
-
-								case ' ':
+							if (map[y][newX] == ' ') {
 								map[y][x] = ' ';
 								x++;
 								map[y][newX] = 'W';
-								break;
-								}
 							}
-						if (GetAsyncKeyState(VK_SPACE) !=0) {
+						}
+						if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
 
 							y--;
 							map[y][x] = '^';
 
-							}
-					break;
+						}
+						if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
+
+							endGame = true;
+
+						}
+						break;
 
 				    case '^':
 
-					
-					map[y][x] = ' ';
-					y--;
-					if (map[y][x] != '#' && map[y][x] != '@') {
-
-						map[y][x] = '^';
-
-					}
-					else if (map[y][x] == '@') {
-
 						map[y][x] = ' ';
+						y--;
+						if (map[y][x] != '#' && map[y][x] != '@') {
+							map[y][x] = '^';
+						}
+						else if (map[y][x] == '@') {
+							map[y][x] = ' ';
+							points++;
+						}
 
-					}
-
-
-					break;
+						break;
 
 					case '@':
 
-						randomNum = rand() % 50 + 1;;
+						randomNum = rand() % 50 + 1;
 
 						if (randomNum == 1) {
 
 							y++;
 							map[y][x] = '*';
 						}
-					break;
+						break;
 
 					case '*':
 
@@ -136,20 +159,32 @@ int main()
 							map[y][x] = '*';
 						}
 						else if (map[y][x] == 'W') {
-							yourhealth -= 20;
+							yourHealth -= 20;
 						}
 						break;
 
 				}
 			}
-			if (yourhealth <= 0) {
-				endgame = true;
+			if (yourHealth <= 0) {
+				yourHealth = 0;
+				if (lives > 0) {
+					lives--;
+					yourHealth = maxHealth;
+					clearConsole();
+					clearBullets();
+					cout << endl << "You were killed" << endl;
+					Sleep(1500);
+				} else {
+					loose = true;
+					endGame = true;
+				}
 			}
 		}
-		Sleep(gamespeed);
+		Sleep(gameSpeed);
 	}
-	system("cls");
-	cout << "You failed to protect the earth from hostile aliens" << endl;
-	system("PAUSE");
+	clearConsole();
+	if (loose) {
+		cout << "You failed to protect the earth from hostile aliens" << endl;
+	}
 	return 0;
 }
